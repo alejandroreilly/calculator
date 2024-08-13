6336edc56d23e281d4953fac9f8ae97131593de2
Alex Reilly <iamalexreilly@gmail.com>
2024-08-12 22:12:35 -0400

    Start making query selectors for the buttons

---
+++ js-file.js
@@ -0,0 +1,5 @@
+//query the buttons
+const clear = document.querySelector("#clearButton");
+const del = document.querySelector("#deleteButton");
+const signChange = document.querySelector("#signChangeButton");
+
