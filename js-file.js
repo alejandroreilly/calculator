8a01f97aa164221af17819d6612ac2312dc341a9
Alex Reilly <iamalexreilly@gmail.com>
2024-08-14 12:07:27 -0400

    Finish button queries and make initial vars

--- js-file.js
+++ js-file.js
@@ -2,4 +2,17 @@
 const clear = document.querySelector("#clearButton");
 const del = document.querySelector("#deleteButton");
 const signChange = document.querySelector("#signChangeButton");
+const opButtons = document.querySelectorAll("#operationButton");
+const numButtons = document.querySelectorAll("#numButton");
+const equal = document.querySelector("#equalButton");
+const decimal = document.querySelector("#decimalButton");
+const display = document.querySelector(".textContainer");
 
+//set input variables
+let displayNum = 0;
+let inputNum1;
+let inputOperator;
+let inputNum2;
+
+//display number on screen
+display.textContent = "displayNum";
