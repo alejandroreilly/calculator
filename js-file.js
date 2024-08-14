b6ce19e455f90d7f87acd8a6675855139a68d213
Alex Reilly <iamalexreilly@gmail.com>
2024-08-14 15:56:32 -0400

    Make numbers and operators functional

--- js-file.js
+++ js-file.js
@@ -10,19 +10,28 @@ const answerDisplay = document.querySelector(".answerContainer");
 const equationDisplay = document.querySelector(".equationContainer");
 
 //set input variables
-let inputNum1;
-let inputOperator;
-let inputNum2;
+let inputNum1 = '';
+let inputOperator = null;
+let inputNum2 = '';
+let needToClearDisplay = false;
 
 //display number on screen
 answerDisplay.textContent=0;
 
 //setup the clear button
 function clearDisplay(){
-    answerDisplay.textContent=0;
-    equationDisplay.textContent="";
+    answerDisplay.textContent='';
+    needToClearDisplay = false;
 }
-clear.addEventListener('click',clearDisplay);
+
+function clearAll(){
+    answerDisplay.textContent='0';
+    equationDisplay.textContent="";
+    inputNum1='';
+    inputOperator=null;
+    inputNum2='';
+}
+clear.addEventListener('click',clearAll);
 
 //setup the signChange
 function changeSign(){
@@ -42,8 +51,75 @@ numButtons.forEach((button) => {
 });
 //add number for the number buttons
 function addNumber(num){
-    if(answerDisplay.textContent === '0'){
+    if(answerDisplay.textContent === '0' || needToClearDisplay){
         answerDisplay.textContent='';
     }
     answerDisplay.textContent+=num;
+};
+
+//setup operator buttons
+opButtons.forEach((button)=>{
+    button.addEventListener('click', () => addOperation(button.textContent));
+});
+function addOperation(operation){
+    if (inputOperator !== null){
+        doMath();
+    }
+    inputNum1 = answerDisplay.textContent;
+    inputOperator=operation;
+    equationDisplay.textContent = `${inputNum1} ${inputOperator}`;
+    needToClearDisplay = true;
+}
+
+//main function for evaluation
+function doMath(){
+    if(inputOperator === null || needToClearDisplay){
+        return
+    }
+    //handle divide by 0
+    if(inputOperator === '/' && answerDisplay.textContent === "0"){
+        alert("You can't divide by 0, silly!");
+        return;
+    }
+
+    inputNum2 = answerDisplay.textContent;
+    answerDisplay.textContent=Math.round(operate(inputOperator, inputNum1, inputNum2) * 1000 / 1000);
+    equationDisplay.textContent = `${inputNum1} ${inputOperator} ${inputNum2} = `;
+    inputOperator = null;
+}
+
+//choose which operation to do
+function operate(operator, one, two){
+    one = Number(one);
+    two = Number(two);
+    switch(operator){
+        case '+':
+            return add(operator, one, two);
+        case '-':
+            return subtract(operator, one, two);
+        case '*':
+            return multiply(operator, one, two);
+        case '/':
+            if(two === 0){
+                return null;
+            }
+            else{
+                return divide(operator, one, two);
+            }
+        default:
+            return null;
+    }
+}
+
+function add(a,b){
+    return a+b;
+}
+function subtract(a,b){
+    return a-b;
+}
+function multiply(a,b){
+    return a*b;
+}
+function divide(a,b){
+    return a/b;
 }
